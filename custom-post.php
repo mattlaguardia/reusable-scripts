5e24ba2ddb1b6ca9bced91717ccfe58c2ac8d08a
mattlaguardia <matthewlaguardia@gmail.com>
2017-04-10 13:14:56 -0700

    creates custom-post.php file for customizable and reusable post types in wordpress

---
+++ custom-post.php
@@ -0,0 +1,4 @@
+<?php
+// NOTE: Don't forget about meta tags
+// NOTE: If statements that looks to see if there is content and posts content if there is
+ ?>
--- wp-query.sql
+++ wp-query.sql
@@ -1,3 +1,3 @@
-UPDATE wp_options SET option_value = replace(option_value, 'http://www.example.com', 'http://localhost/test-site') WHERE option_name = 'home' OR option_name = 'siteurl';
-UPDATE wp_posts SET post_content = replace(post_content, 'http://www.example.com', 'http://localhost/test-site');
-UPDATE wp_postmeta SET meta_value = replace(meta_value,'http://www.example.com','http://localhost/test-site');
+UPDATE wp_options SET option_value = replace(option_value, 'http://acclivityassociates.com/', 'http://localhost/clients/template_a/') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE wp_posts SET post_content = replace(post_content, 'http://acclivityassociates.com/', 'http://localhost/clients/template_a/');
+UPDATE wp_postmeta SET meta_value = replace(meta_value,'http://acclivityassociates.com/','http://localhost/clients/template_a/');
